046371a1df02cec1308750fb41e85a61ad254140
Francisco Surroca <fsurroca@francisco-surrocas-macbook-pro.local>
2013-12-11 22:33:24 -0500

    Initial Commit

---
+++ ImagesColide/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ImagesColide
+//
+//  Created by Francisco Surroca on 12/11/13.
+//  Copyright (c) 2013 Francisco Surroca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ImagesColide/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ImagesColide
+//
+//  Created by Francisco Surroca on 12/11/13.
+//  Copyright (c) 2013 Francisco Surroca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
